b9add99989e5e228810a6a23bee5cd7a9e666466
ten <tenkung10@hotmail.com>
2015-04-07 21:42:14 +0700

    edit Product Repository

--- src/main/java/camt/se331/shoppingcart/repository/productRepository.java
+++ src/main/java/camt/se331/shoppingcart/repository/productRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 /**
  * Created by TEN10 on 4/7/2015.
  */
-public interface ProductRepository extends JpaRepository<Product,Long>{
+public interface ProductRepository extends JpaRepository<Product,Long> {
 }
